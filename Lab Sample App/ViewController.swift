b18c915f247b1db9d4f798970d3e982751cd1077
Tobias Weidinger <tobias.weidinger@gmx.de>
2017-02-21 15:08:42 +0100

    added sample project

---
+++ Lab Sample App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab Sample App
+//
+//  Created by Tobias Weidinger on 21/02/2017.
+//  Copyright © 2017 PDLB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
